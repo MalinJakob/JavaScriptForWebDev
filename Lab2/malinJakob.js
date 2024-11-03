4074fbc75565e800956a7a970c5156158a68c0f5
Malin Jakob <malin.jakob98@gmail.com>
2024-11-03 20:43:47 +0100

    add object for quiz answers

--- Lab2/malinJakob.js
+++ Lab2/malinJakob.js
@@ -1,5 +1,29 @@
-//validate email and name for right format, else display a message
+const quiz = {
+  q1: {
+    africanForestElephant: false,
+    africanBushElephant: true,
+    asianElephant: false
+  },
+  q2: {
+    keenSenseOfSmell: true,
+    formHerds: true,
+    herbivores: true,
+    useTrunks: true
+  },
+  q3: {
+    min: 60,
+    max: 70
+  },
+  q4: {
+    climbingTrees: false,
+    rollingInMud: true,
+    diggingBurrows: false,
+    divingUnderWater: false
+  },
+  q5: ['Poaching for ivory', 'habitat loss', 'human-wildlife conflict']
+}
 
+//validate email and name for right format, else display a message
 function validEmail (email) {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   return emailPattern.test(email)
@@ -8,7 +32,7 @@ function validEmail (email) {
 document
   .getElementById('quizForm')
   .addEventListener('submit', function (event) {
-
+    event.preventDefault()
     //get input value from form and remove white space
     const email = document.getElementById('email').value.trim()
     const invalidEmailMessage = document.getElementById('invalidEmail')
@@ -17,7 +41,6 @@ document
     if (validEmail(email)) {
       invalidEmailMessage.textContent = ''
     } else {
-      event.preventDefault()
       invalidEmailMessage.textContent = 'Please enter a valid email address!'
       document.getElementById('email').focus()
     }
