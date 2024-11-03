7805568e64f0115d47c39ea5f52982b97e8131f3
Malin Jakob <malin.jakob98@gmail.com>
2024-11-03 15:45:49 +0100

    fix the validation for email so the submition can happen if everything is valid

--- Lab2/malinJakob.js
+++ Lab2/malinJakob.js
@@ -1,25 +1,24 @@
 //validate email and name for right format, else display a message
 
 function validEmail (email) {
-  const emailPattern =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   return emailPattern.test(email)
 }
 
-document.getElementById('quizForm').addEventListener('submit', function(event){
-  event.preventDefault()
+document
+  .getElementById('quizForm')
+  .addEventListener('submit', function (event) {
 
-  //get input value from form and remove white space
-  const email = document.getElementById('email').value.trim()
-  const invalidEmailMessage = document.getElementById('invalidEmail')
+    //get input value from form and remove white space
+    const email = document.getElementById('email').value.trim()
+    const invalidEmailMessage = document.getElementById('invalidEmail')
 
-  //call the check email function and add message if not valid
-  if(!validEmail(email)){
-    invalidEmailMessage.textContent = ''
-    invalidEmailMessage.textContent = 'Please enter a valid email address!'
-    document.getElementById('email').focus()
-  }
-})
-
-
-
-
+    //call the check email function and add message if not valid
+    if (validEmail(email)) {
+      invalidEmailMessage.textContent = ''
+    } else {
+      event.preventDefault()
+      invalidEmailMessage.textContent = 'Please enter a valid email address!'
+      document.getElementById('email').focus()
+    }
+  })
